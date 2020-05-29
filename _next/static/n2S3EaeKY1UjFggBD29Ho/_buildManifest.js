d92ef299cd9dac254f8b009ca35759a2ad0bdddf
denyo <denyo@users.noreply.github.com>
2020-05-29 11:33:12 +0000

    Automated deployment of example (master): 0301da5ed21ac4147e0f6014bef4041c9033692c

---
+++ _next/static/n2S3EaeKY1UjFggBD29Ho/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/collection":[a],"/forms":[a],"/index":[a],"/phn-header":[a]}}("static\u002Fchunks\u002F30b1b6e243727b30c613a6990b011b3c680491cc.9844ed1571c56adac546.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
