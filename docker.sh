ccc83d660a967780b49a7ef760894ef8d3aaa2d8
Bernd Hacker <hacker.bernd@gmail.com>
2020-05-29 11:09:47 +0200

    ci: add git workflow, Dockerfile and docker-compose.yml | bh

---
+++ docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+SCRIPT_DIR="$(cd `dirname ${0}` && pwd)"
+
+export RUN_UID="$(id -u)"
+export RUN_GID="$(id -g)"
+export COMPOSE_PROJECT_NAME="sample-integration-nextjs"
+
+docker-compose -f "${SCRIPT_DIR}/docker-compose.yml" build
+docker-compose -f "${SCRIPT_DIR}/docker-compose.yml" run --rm change-volume-owner
+docker-compose -f "${SCRIPT_DIR}/docker-compose.yml" run --service-ports --rm "sample-integration" "${@}"
