5b5ddfe71e05cc9113f4bd3107ccd1cd6b0c637e
Oliver Hook <oliver.hook@mhp.com>
2020-07-30 15:38:25 +0200

    add notification banner | oh | #514

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from "react";
+import { CDN_BASE_URL, JS_MANIFEST } from '@porsche-design-system/browser-notification-banner';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -9,11 +10,12 @@ class MyDocument extends Document {
 
   render() {
     return (
-    <Html>
+    <Html lang="en">
       <Head>
         <meta charSet="utf-8"/>
         <link rel="icon" href="/favicon.ico" key="favicon" />
         <link rel="preconnect" href="https://cdn.ui.porsche.com/" key="porscheui-cdn-preconnect"/>
+        <script defer src={`${CDN_BASE_URL}/${JS_MANIFEST.init}`} key="browser-notification-banner"/>
       </Head>
       <body>
       <Main />
