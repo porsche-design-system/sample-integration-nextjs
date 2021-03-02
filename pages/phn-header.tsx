08b573330c3d9ab12548822d33640b5e5066268d
Matthias Haegele <matthias.haegele@mhp.com>
2021-03-02 08:37:21 +0100

    fix: remove phn header from SSR until supported | mh

--- pages/phn-header.tsx
+++ pages/phn-header.tsx
@@ -1,19 +1,18 @@
 import { PButton, PIcon, PLinkPure } from '@porsche-design-system/components-react';
-import { applyPolyfills, defineCustomElements as definePorscheNavigation } from '@porschehn/navigation/loader';
 import React from 'react';
 import Header from '../components/header';
 import Head from 'next/head';
 
-applyPolyfills().then(() => {
+/*applyPolyfills().then(() => {
   definePorscheNavigation(window);
-});
+});*/
 
 const PhnHeaderPage = (): JSX.Element => (
   <div className="pageLayout">
     <Head>
       <title>NextJS sample project - PHN Header</title>
     </Head>
-    <phn-header env="preview" style={{ position: 'absolute', left: 0, right: 0 }} />
+{/*    <phn-header env="preview" style={{ position: 'absolute', left: 0, right: 0 }} />*/}
     <div style={{ paddingTop: 180 }}>
       <Header />
       <div>
