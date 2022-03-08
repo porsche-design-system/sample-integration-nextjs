df5991f31995f6524881cc91126d24f638bfd498
Bernd Hacker <hacker.bernd@gmail.com>
2022-03-08 12:43:28 +0100

    style: improve formatting | bh | #1793

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
 import {
-    getFontFaceStylesheet,
-    getInitialStyles,
-    getLoaderScript,
-    getIconLinks,
-    getMetaTagsAndIconLinks, getComponentChunkLinks, getFontLinks
+  getFontFaceStylesheet,
+  getInitialStyles,
+  getLoaderScript,
+  getIconLinks,
+  getMetaTagsAndIconLinks,
+  getComponentChunkLinks,
+  getFontLinks,
 } from '@porsche-design-system/components-react/partials';
 import { includeOverlay, includeCookieOverlay, includeBanner } from '@porsche-design-system/browser-notification';
-
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -22,17 +23,17 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <link rel="icon" href="/favicon.ico" key="favicon" />
-          {getFontFaceStylesheet({format: 'jsx'})}
-          {getInitialStyles({format: 'jsx'})}
-          {getComponentChunkLinks({format: 'jsx'})}
-          {getFontLinks({format: 'jsx'})}
-          {getIconLinks({format: 'jsx'})}
-          {getMetaTagsAndIconLinks({ appTitle: 'Sample Project NextJS', format: 'jsx'})}
+          {getFontFaceStylesheet({ format: 'jsx' })}
+          {getInitialStyles({ format: 'jsx' })}
+          {getComponentChunkLinks({ format: 'jsx' })}
+          {getFontLinks({ format: 'jsx' })}
+          {getIconLinks({ format: 'jsx' })}
+          {getMetaTagsAndIconLinks({ appTitle: 'Sample Project NextJS', format: 'jsx' })}
         </Head>
         <body>
           <Main />
           <NextScript />
-          {getLoaderScript({format: 'jsx'})}
+          {getLoaderScript({ format: 'jsx' })}
           <div dangerouslySetInnerHTML={{ __html: includeBanner() }} />
           <div dangerouslySetInnerHTML={{ __html: includeCookieOverlay() }} />
           <div dangerouslySetInnerHTML={{ __html: includeOverlay() }} />
