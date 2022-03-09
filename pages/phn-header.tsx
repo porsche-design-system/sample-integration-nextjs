9650698f027f1abf53bf62a663f69965e63fcfae
Bernd Hacker <hacker.bernd@gmail.com>
2022-03-09 11:43:16 +0100

    perf: initialize phn-header via useEffect | bh | #1793

--- pages/phn-header.tsx
+++ pages/phn-header.tsx
@@ -1,35 +1,37 @@
 import { PButton, PIcon, PLinkPure } from '@porsche-design-system/components-react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/header';
 import Head from 'next/head';
-import { applyPolyfills, defineCustomElements as definePorscheNavigation } from '@porschehn/navigation/loader';
+import { applyPolyfills, defineCustomElements } from '@porschehn/navigation/loader';
 
-applyPolyfills().then(() => {
-  if (typeof window !== 'undefined') {
-    definePorscheNavigation(window);
-  }
-});
+const PhnHeaderPage = (): JSX.Element => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      applyPolyfills().then(() => defineCustomElements(window));
+    }
+  }, []);
 
-const PhnHeaderPage = (): JSX.Element => (
-  <div className="pageLayout">
-    <Head>
-      <title>NextJS sample project - PHN Header</title>
-    </Head>
-    <phn-header env="preview" style={{ position: 'absolute', left: 0, right: 0 }} />
+  return (
+    <div className="pageLayout">
+      <Head>
+        <title>NextJS sample project - PHN Header</title>
+      </Head>
+      <phn-header env="preview" style={{ position: 'absolute', left: 0, right: 0 }} />
 
-    <div style={{ paddingTop: 180 }}>
-      <Header />
-      <PButton>Some PButton</PButton>
-      <br />
-      <br />
-      <PLinkPure href="https://porsche.com">Some PLinkPure</PLinkPure>
-      <br />
-      <br />
-      <PIcon name="configurate" />
-      <PIcon name="compare" />
-      <PIcon name="search" />
+      <div style={{ paddingTop: 180 }}>
+        <Header />
+        <PButton>Some PButton</PButton>
+        <br />
+        <br />
+        <PLinkPure href="https://porsche.com">Some PLinkPure</PLinkPure>
+        <br />
+        <br />
+        <PIcon name="configurate" />
+        <PIcon name="compare" />
+        <PIcon name="search" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default PhnHeaderPage;
