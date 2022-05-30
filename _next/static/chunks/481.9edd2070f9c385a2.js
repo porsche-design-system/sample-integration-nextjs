1f2351462f309092ccdc91fe88263159847757a9
stephanschroeter <stephanschroeter@users.noreply.github.com>
2022-05-30 12:28:13 +0000

    Automated deployment of example (master): 7224ffc7b5fab46317fb4c27e1645682675db7a6

---
+++ _next/static/chunks/481.9edd2070f9c385a2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[481],{4481:function(s,e,n){n.r(e),n.d(e,{b:function(){return u}});var u={xxs:0,xs:480,s:760,m:1e3,l:1300,xl:1760,xxl:1920}}}]);
