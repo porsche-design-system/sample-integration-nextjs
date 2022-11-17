6f0e87b18c2bccc59eb4e6fbdd49b6094af1fd1f
Stephan Schröter <stephan.schroeter@mhp.com>
2022-11-17 20:20:34 +0100

    feat: improve partial integration | sas

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,21 +24,30 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <link rel="icon" href="/favicon.ico" key="favicon" />
-          {getInitialStyles({ format: 'jsx'})} // **necessary for SSR support**, inject stylesheet which defines visibility of pre-hydrated PDS components
-          {getFontFaceStylesheet({ format: 'jsx' })} // inject stylesheet which defines Porsche Next CSS font-face definition
-          {getFontLinks({ format: 'jsx' })} // preloads Porsche Next font
-          {getComponentChunkLinks({ format: 'jsx' })} // preloads PDS component core chunk from CDN for PDS component hydration
-          {getIconLinks({ format: 'jsx' })} // preloads Porsche icons
-          {getMetaTagsAndIconLinks({ appTitle: 'Sample Project NextJS', format: 'jsx' })} // inject favicon, apple touch icons, android touch icons, etc.
+          {/* **necessary for SSR support**, injects stylesheet which defines visibility of pre-hydrated PDS components */}
+          {getInitialStyles({ format: 'jsx'})}
+          {/* injects stylesheet which defines Porsche Next CSS font-face definition (=> minimize FOUT) */}
+          {getFontFaceStylesheet({ format: 'jsx' })}
+          {/* preloads Porsche Next font (=> minimize FOUT) */}
+          {getFontLinks({ format: 'jsx' })}
+          {/* preloads PDS component core chunk from CDN for PDS component hydration (=> improve loading performance) */}
+          {getComponentChunkLinks({ format: 'jsx' })}
+          {/* preloads Porsche icons (=> minimize FOUC) */}
+          {getIconLinks({ format: 'jsx' })}
+          {/* injects favicon, apple touch icons, android touch icons, etc. */}
+          {getMetaTagsAndIconLinks({ appTitle: 'Sample Project NextJS', format: 'jsx' })}
         </Head>
         <body>
           <Main />
           <NextScript />
           {/* getLoaderScript partial makes no sense in this context and breaks the app in dev mode */}
           {/* {getLoaderScript({ format: 'jsx' })} */}
-          {getDSRPonyfill({ format: 'jsx' })} // **necessary for SSR support**, enables declarative shadow dom support for Safari and Firefox
-          {getCookiesFallbackScript({ format: 'jsx' })} // shows a cookie fallback overlay and blocks the page, in case cookies are disabled
-          {getBrowserSupportFallbackScript({ format: 'jsx' })} // shows a browser fallback overlay and blocks the page, in case browser is not supported (e.g. IE11)
+          {/* **necessary for SSR support**, enables declarative shadow dom support for Safari and Firefox */}
+          {getDSRPonyfill({ format: 'jsx' })}
+          {/* shows a cookie fallback overlay and blocks the page, in case cookies are disabled */}
+          {getCookiesFallbackScript({ format: 'jsx' })}
+          {/* shows a browser fallback overlay and blocks the page, in case browser is not supported (e.g. IE11) */}
+          {getBrowserSupportFallbackScript({ format: 'jsx' })}
         </body>
       </Html>
     );
