f84683d3569e1c19796ebcc2cfdc5eb5fe4384e1
Stephan Schröter <stephan.schroeter@mhp.com>
2022-11-17 20:14:08 +0100

    feat: improve partial integration | sas

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,21 +24,21 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <link rel="icon" href="/favicon.ico" key="favicon" />
-          {getFontFaceStylesheet({ format: 'jsx' })}
-          {getComponentChunkLinks({ format: 'jsx' })}
-          {getFontLinks({ format: 'jsx' })}
-          {getIconLinks({ format: 'jsx' })}
-          {getMetaTagsAndIconLinks({ appTitle: 'Sample Project NextJS', format: 'jsx' })}
-          {getInitialStyles({ format: 'jsx'})}
+          {getInitialStyles({ format: 'jsx'})} // **necessary for SSR support**, inject stylesheet which defines visibility of pre-hydrated PDS components
+          {getFontFaceStylesheet({ format: 'jsx' })} // inject stylesheet which defines Porsche Next CSS font-face definition
+          {getFontLinks({ format: 'jsx' })} // preloads Porsche Next font
+          {getComponentChunkLinks({ format: 'jsx' })} // preloads PDS component core chunk from CDN for PDS component hydration
+          {getIconLinks({ format: 'jsx' })} // preloads Porsche icons
+          {getMetaTagsAndIconLinks({ appTitle: 'Sample Project NextJS', format: 'jsx' })} // inject favicon, apple touch icons, android touch icons, etc.
         </Head>
         <body>
           <Main />
           <NextScript />
           {/* getLoaderScript partial makes no sense in this context and breaks the app in dev mode */}
           {/* {getLoaderScript({ format: 'jsx' })} */}
-          {getDSRPonyfill({ format: 'jsx' })}
-          {getCookiesFallbackScript({ format: 'jsx' })}
-          {getBrowserSupportFallbackScript({ format: 'jsx' })}
+          {getDSRPonyfill({ format: 'jsx' })} // **necessary for SSR support**, enables declarative shadow dom support for Safari and Firefox
+          {getCookiesFallbackScript({ format: 'jsx' })} // shows a cookie fallback overlay and blocks the page, in case cookies are disabled
+          {getBrowserSupportFallbackScript({ format: 'jsx' })} // shows a browser fallback overlay and blocks the page, in case browser is not supported (e.g. IE11)
         </body>
       </Html>
     );
