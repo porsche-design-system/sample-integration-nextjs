dead6bb2fe711ef2bcf75e37f2e38a75807df84b
Henri Joss <henri.joss@mhp.com>
2024-05-06 15:23:44 +0200

    feat: add partials | hj | #3137

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,38 @@
-import {PorscheDesignSystemProvider, PToast} from '@porsche-design-system/components-react/ssr';
+import { PorscheDesignSystemProvider, PToast } from '@porsche-design-system/components-react/ssr';
 import {
-  getBrowserSupportFallbackScript, getComponentChunkLinks,
+  getBrowserSupportFallbackScript,
+  getComponentChunkLinks,
   getCookiesFallbackScript,
-  getDSRPonyfill, getFontLinks, getIconLinks, getMetaTagsAndIconLinks,
+  getDSRPonyfill,
+  getFontLinks,
+  getIconLinks,
+  getMetaTagsAndIconLinks,
 } from '@porsche-design-system/components-react/partials';
-import React from 'react';
-import {Header, Partials} from '../components';
+import { preload, prefetchDNS } from 'react-dom';
+import { Header, Partials } from '../components';
 import '../styles.scss';
-import {Metadata} from "next";
+import { Metadata, Viewport } from 'next';
 
+const title = 'NextJS sample project';
+
+const { themeColor, appleWebApp, icons, manifest } = getMetaTagsAndIconLinks({
+  appTitle: title,
+  format: 'js',
+});
+
+/* injects meta theme-color. */
+export const viewport: Viewport = {
+  themeColor,
+};
+
+/* injects favicon, apple touch icons, android touch icons, etc. */
 export const metadata: Metadata = {
-  /* injects favicon, apple touch icons, android touch icons, etc. */
-  // ...getMetaTagsAndIconLinks({ format: "js" });
-}
+  title,
+  appleWebApp,
+  icons,
+  // Next currently automatically applies `crossorigin="use-credentials"` to the link which causes a CORS Error in the manifest link (Will be fixed in upcoming Next version)
+  // manifest,
+};
 
 export default function RootLayout({
   children,
@@ -20,13 +40,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   /* preloads Porsche Next font (=> minimize FOUT) */
-  // getFontLinks({ format: 'js' }).forEach(({ href, ...opts }) => ReactDOM.preload(href, opts));
+  getFontLinks({ format: 'js' }).forEach(({ href, options }) => preload(href, options));
   /* preloads PDS component core chunk from CDN for PDS component hydration (=> improve loading performance) */
-  // getComponentChunkLinks({ components: ['tabs-bar', 'scroller', 'icon'], format: 'js' }).forEach(
-  //   ({ href, ...opts }) => ReactDOM.preload(href, opts)
-  // );
+  getComponentChunkLinks({ components: ['tabs-bar', 'scroller', 'icon'], format: 'js' }).forEach(({ href, options }) =>
+    preload(href, options)
+  );
   /* preloads Porsche icons (=> minimize FOUC) */
-  // getIconLinks({ format: 'js' }).forEach(ReactDOM.prefetchDNS);
+  getIconLinks({ format: 'js' }).forEach(({ href }) => prefetchDNS(href));
 
   return (
     <html lang="en">
